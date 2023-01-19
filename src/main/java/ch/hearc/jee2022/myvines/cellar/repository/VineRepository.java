6eb0c00dcb6a73a136ca3a472b0b8e06287fd45f
Cyrille Polier <cyrille.polier@he-arc.ch>
2023-01-19 15:22:30 +0100

    update repository

--- src/main/java/ch/hearc/jee2022/myvines/cellar/repository/VineRepository.java
+++ src/main/java/ch/hearc/jee2022/myvines/cellar/repository/VineRepository.java
@@ -1,5 +1,8 @@
 package ch.hearc.jee2022.myvines.cellar.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -7,4 +10,9 @@ import ch.hearc.jee2022.myvines.cellar.model.Vine;
 
 public interface VineRepository extends CrudRepository<Vine, Long>, PagingAndSortingRepository<Vine, Long> {
 
+	@Query("SELECT v FROM Vine v WHERE v.user.id = ?1")
+	public Page<Vine> findVineByUser(Long userId, Pageable pageable);
+	
+	@Query("SELECT v FROM Vine WHERE TRUE")
+	public Page<Vine> findAllVines(Pageable pageable);
 }
